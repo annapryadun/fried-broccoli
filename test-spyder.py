0d5e72faa3f0aa1d32892cc1ffd3de363d1bf34f
annapryadun <112638500+annapryadun@users.noreply.github.com>
2024-03-20 17:29:18 +0100

    Create test-spyder.py

---
+++ test-spyder.py
@@ -0,0 +1,11 @@
+# Define multiple variables
+name = "Alice"
+age = 30
+city = "New York"
+is_student = False
+
+# Print the variables
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Is student:", is_student)
